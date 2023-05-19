e81b603f19e305a7917fe49133f89022e81119cf
surendrapk <115524115+surendraanusha@users.noreply.github.com>
2023-05-19 15:10:49 +0530

    required packages are installed

---
+++ src/app/dashboard/settings/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Seetings = () => {
+  return (
+    <div>
+      <p>Hello settings</p>
+    </div>
+  )
+}
+
+export default Seetings
---
+++ src/app/products/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <p>product id is 1</p>
+    </div>
+  )
+}
+
+export default page
